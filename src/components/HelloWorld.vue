8d75b27b66e00b11b47440afba30ea3bc8ea8e58
huchangkuang <603021137@qq.com>
2020-07-23 15:29:10 +0800

    封装axios到request函数

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,26 @@
 <template>
   <div class="hello">
+    <el-input v-model="input" placeholder="请输入内容"></el-input>
     <a href="#">hello</a>
+    <el-button :plain="true" @click="success">成功</el-button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return{
+      input: ""
+    }
+  },
+  methods:{
+    success(){
+      this.$message({
+        message:"这是一条消息",
+        type:"success"
+      })
+    }
   }
 }
 </script>
